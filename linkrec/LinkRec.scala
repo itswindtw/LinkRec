ae9fe47dacee7fa22d5ca1fa919d6840f00af42e
Meng-Hsin Tung <itswindtw@gmail.com>
2015-04-03 12:33:05 -0400

    improve?

--- linkrec/LinkRec.scala
+++ linkrec/LinkRec.scala
@@ -10,12 +10,13 @@ import org.apache.hadoop.hbase.{HBaseConfiguration, HTableDescriptor, TableName}
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.CellUtil
 import org.apache.hadoop.hbase.util.Bytes
-// import org.apache.hadoop.hbase.mapred.TableInputFormat
-// import org.apache.hadoop.mapred.JobConf
+
+case class MyRating(userId: Int, url: String, title: String, time: Long)
+case class MyLink(url: String, title: String) extends Ordered[MyLink] {
+  def compare(another: MyLink): Int = this.url compare another.url
+}
 
 object LinkRec {
-  case class MyRating(userId: Int, product: String, rating: Double);
-
   val logger = Logger("LinkRec")
 
   def main(args: Array[String]) {
@@ -25,27 +26,28 @@ object LinkRec {
     }
 
     // set up environment
-    val conf = new SparkConf().setAppName("LinkRec")
-    val sc = new SparkContext(conf)
+    val conf: SparkConf = new SparkConf().setAppName("LinkRec")
+    val sc: SparkContext = new SparkContext(conf)
 
-    // load data, data in tuple (user: String, url: String, title: String, time: Long)
+    // load data, data in MyRating
     logger.warn("start loading data")
-    val data = loadDataFromDB(sc)
+    val trainingData: RDD[MyRating] = loadDataFromDB(sc).cache()
     logger.warn("complete loading data")
     // println("[XC] Data: ")
     // data.collect().foreach(println)
 
-    // get training data
-    val trainingData = data.map(tuple => MyRating(tuple._1, tuple._2, 1.0))
+    // create mapping
+    val allLinks = trainingData.map(r => MyLink(r.url, r.title)).distinct()
 
-    // Create mapping
-    val productToInt: RDD[(String, Long)] = trainingData.map(_.product).distinct().zipWithUniqueId()
-    val intToProduct: RDD[(Long, String)] = productToInt map { case (l, r) => (r, l) }
-    val map: Map[String, Int] = productToInt.mapValues(_.toInt).collect().toMap
-    val reversedMap: Map[Long, String] = intToProduct.collect().toMap
+    val linkToInt: RDD[(MyLink, Long)] = allLinks.zipWithUniqueId()
+    val intToLink: RDD[(Long, MyLink)] = linkToInt map { case (l, r) => (r, l) }
+
+    // FIXME: I don't know how to annnotate Map here..
+    val linkMap = linkToInt.collectAsMap map { case (l, r) => (l, r.toInt) }
+    val reversedLinkMap = intToLink.collectAsMap
 
     val trainingRating: RDD[Rating] = trainingData.map { r =>
-      Rating(r.userId, map(r.product), r.rating)
+      Rating(r.userId, linkMap(MyLink(r.url, r.title)), 1.0)
     }
 
     // println("[XC] trainingData: ")
@@ -53,41 +55,32 @@ object LinkRec {
 
     // Build the recommendation model using ALS
     logger.warn("start training data")
-    var model = getBestModel(trainingRating)
+    var model: MatrixFactorizationModel = getBestModel(trainingRating)
     logger.warn("complete training data")
 
     // get target user id
     val userId = args(0).toInt
 
-    val sharedLinks = trainingRating.filter(_.user == userId).map(_.product)
-    val allLinks = trainingRating.map(_.product).distinct()
+    val sharedLinks = trainingData.filter(_.userId == userId).map(r => MyLink(r.url, r.title))
     val unsharedLinks = allLinks.subtract(sharedLinks)
 
-    val userData = unsharedLinks.map((userId, _))
+    val userData = unsharedLinks.map(l => (userId, linkMap(l)))
 
     // println("[XC] User Data: ")
     // userData.collect().foreach(println)
 
     // recommendation
     logger.warn("start prediction")
-    val predictions = model.predict(userData).collect().filter(_.rating >= 0).sortBy(-_.rating).take(50)
+    val predictions = model.predict(userData).filter(_.rating >= 0).sortBy(-_.rating).take(50)
     logger.warn(predictions.mkString("\n"))
 
-    val reclinks = predictions.map(_.product).map { p =>
-      reversedMap(p)
-    }
+    var reclinks = predictions.map(p => reversedLinkMap(p.product))
 
     // ranking TODO
     logger.warn("start building map")
-    var linkTitleMap = data.filter(tuple => reclinks.contains(tuple._2))
-                           .map(tuple => (tuple._2, tuple._3))
-                           .distinct()
-                           .collectAsMap()
 
-    logger.warn(linkTitleMap.mkString("\n"))
-
-    var reclinksWithTitle = reclinks.map(url =>
-                            "{\"url\":\"" + url + "\", \"title\":\"" + linkTitleMap.get(url).get + "\"}");
+    var reclinksWithTitle = reclinks.map(link =>
+                            "{\"url\":\"" + link.url + "\", \"title\":\"" + link.title + "\"}");
 
     print("{\"reclinks\": [" + reclinksWithTitle.mkString(", ") + "]}")
 
@@ -98,31 +91,23 @@ object LinkRec {
     sc.stop()
   }
 
-  def loadDataFromDB(sc: SparkContext): RDD[(Int, String, String, Long)] = {
+  def loadDataFromDB(sc: SparkContext): RDD[MyRating] = {
     val conf = HBaseConfiguration.create()
-
     conf.set(TableInputFormat.INPUT_TABLE, "linkrec")
-    // conf.set(TableInputFormat.SCAN_COLUMN_FAMILY, "link")
-
-    // conf.set("hbase.mapred.inputtable", "linkrec")
-    // conf.set("hbase.mapred.tablecolumns", "link")
-
-    // val hBaseRDD = sc.hadoopRDD(new JobConf(conf), classOf[TableInputFormat],
-    //   classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
-    //   classOf[org.apache.hadoop.hbase.client.Result])
 
     val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat],
       classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
       classOf[org.apache.hadoop.hbase.client.Result])
 
-    val ratings = hBaseRDD.map(_._2).map(_.raw())
-                  .flatMap(_.map( cell => (
-                          Bytes.toString(CellUtil.cloneRow(cell)).toInt,
-                          Bytes.toString(CellUtil.cloneQualifier(cell)),
-                          Bytes.toString(CellUtil.cloneValue(cell)),
-                          cell.getTimestamp()) ))
+    val ratings = hBaseRDD.map(item => item._2.raw())
+                    .flatMap(_.map (cell =>
+                      MyRating(
+                        Bytes.toString(CellUtil.cloneRow(cell)).toInt,
+                        Bytes.toString(CellUtil.cloneQualifier(cell)),
+                        Bytes.toString(CellUtil.cloneValue(cell)),
+                        cell.getTimestamp())))
 
-    return ratings.cache()
+    return ratings
   }
 
   def getBestModel(data: RDD[Rating]): MatrixFactorizationModel = {
